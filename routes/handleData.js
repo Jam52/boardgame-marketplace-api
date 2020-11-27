7db588861a007252dc16c52a6a26e751a841718b
Jam52 <jamie.sajdak@gmail.com>
2020-11-27 14:12:48 -0500

    Add categorie, mechanic, payer count and play time to filter

--- routes/handleData.js
+++ routes/handleData.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const _ = require('lodash');
 
 const fetchData = async (type, query) => {
   console.log('[fetchData]');
@@ -27,10 +28,41 @@ const fetchData = async (type, query) => {
   return returnGameData;
 };
 
-const filterData = (data) => {
-  const filteredGames = data.games.slice(0, 30);
-  data.games = filteredGames;
-  return data;
+const filterData = (data, queries) => {
+  let gameData = _.clone(data);
+  console.log(queries);
+  let entries = Object.entries(queries);
+  entries.forEach((query) => {
+    const [key, value] = query;
+    if (value !== '') {
+      console.log(value);
+      if (key === 'mechanics') {
+        gameData.games = gameData.games.filter((game) => {
+          const ids = game.mechanics.map((obj) => obj.id);
+          return String(ids).includes(value);
+        });
+      }
+      if (key === 'categories') {
+        gameData.games = gameData.games.filter((game) => {
+          const ids = game.categories.map((obj) => obj.id);
+          return String(ids).includes(value);
+        });
+      }
+      if (key === 'player_count') {
+        gameData.games = gameData.games.filter((game) => {
+          return game.min_players <= value && game.max_players >= value;
+        });
+      }
+      if (key === 'play_time') {
+        gameData.games = gameData.games.filter((game) => {
+          return game.min_playtime <= value && game.max_playtime >= value;
+        });
+      }
+    }
+  });
+  const filteredGames = gameData.games.slice(0, 30);
+  gameData.games = filteredGames;
+  return gameData;
 };
 
 module.exports = {
--- routes/index.js
+++ routes/index.js
@@ -20,25 +20,40 @@ router.get('/search', async (req, res) => {
     player_count: req.query.player_count,
     play_time: req.query.play_time,
     year_published: req.query.year_published,
-    order_by: req.query.order_by,
   };
+
+  // const [order_by] = req.query;
 
   const keyArry = Object.keys(queries);
 
-  // for()
+  for (let i = 0; i < keyArry.length; i++) {
+    if (queries[keyArry[i]] !== '') {
+      const mainCategory = queries[keyArry[i]].split(',')[0];
+      if (cashedData[mainCategory] === undefined) {
+        console.log('Fetching');
+        const data = await fetchData([keyArry[i]], mainCategory);
+        const dataObj = {
+          games: data,
+          length: data.length,
+        };
+        cashedData[mainCategory] = dataObj;
+        try {
+          res.send(filterData(dataObj, queries));
+        } catch (error) {
+          console.log(error);
+        }
 
-  if (queries.categories !== '') {
-    const mainCategory = queries.categories.split(',')[0];
-    if (cashedData[mainCategory] === undefined) {
-      const data = await fetchData('categories', mainCategory);
-      const dataObj = {
-        games: data,
-        length: data.length,
-      };
-      cashedData[mainCategory] = dataObj;
-      res.send(filterData(dataObj));
-    } else {
-      res.send(filterData(cashedData[mainCategory]));
+        break;
+      } else {
+        console.log('returning from cash');
+        try {
+          res.send(filterData(cashedData[mainCategory], queries));
+        } catch (error) {
+          console.log(error);
+        }
+
+        break;
+      }
     }
   }
 });
